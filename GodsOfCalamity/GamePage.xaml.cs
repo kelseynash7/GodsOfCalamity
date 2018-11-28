455d58499c22535abfd512562ba55fff20867f04
KelseyNash <kelsey.nash@wsu.edu>
2018-11-27 16:41:23 -0800

    first push

---
+++ GodsOfCalamity/GamePage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GodsOfCalamity
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GamePage : Page
+    {
+        private List<UserControl> enemies = new List<UserControl>();
+        private int maxEnemies = 10;
+        private DispatcherTimer timer = new DispatcherTimer();
+        private Random randomizer = new Random();
+        private int Level { get; set; }
+
+        public GamePage()
+        {
+            this.InitializeComponent();
+
+            Loaded += (sender, args) =>
+            {
+                timer.Tick += TimerOnTick;
+                timer.Interval = new TimeSpan(0, 0, 0, 2);
+                timer.Start();
+            };
+
+            CompositionTarget.Rendering += GameLoop;
+        }
+
+        private void TimerOnTick(object sender, object o)
+        {
+            if (enemies.Count < maxEnemies)
+            {
+                var enemyType = randomizer.Next(1, 2);
+                if (enemyType == 1)
+                {
+                    var enemy = new Meteor
+                    {
+                        AreaWidth = (int)LayoutRoot.ActualWidth,
+                        Location =
+                        new Point(randomizer.Next(0, (int)LayoutRoot.ActualWidth - 80), 0)
+                    };
+
+                    enemy.Velocity = enemy.Velocity * ((Level / (double)10) + 1);
+                    enemies.Add(enemy);
+                    Canvas.SetZIndex(enemy, 5);
+                    LayoutRoot.Children.Add(enemy);
+                }
+                else
+                {
+                    var enemy = new Fire()
+                    {
+                        AreaWidth = (int)LayoutRoot.ActualWidth,
+                        Location =
+                            new Point(randomizer.Next(0, (int)LayoutRoot.ActualWidth - 80), 0)
+                    };
+
+                    enemy.PropagationRate = enemy.PropagationRate * ((Level / (double)10) + 1);
+                    enemies.Add(enemy);
+                    Canvas.SetZIndex(enemy, 5);
+                    LayoutRoot.Children.Add(enemy);
+                }
+                
+            }
+        }
+
+        private void GameLoop(object sender, object e)
+        {
+            //move enemies
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                enemies[i].Move();
+                enemies[i].Margin = new Thickness(enemies[i].Location.X, enemies[i].Location.Y, 0, 0);
+
+                if (enemies[i].Margin.Top > App.ScreenHeight)
+                {
+                    LayoutRoot.Children.Remove(enemies[i]);
+                    enemies.Remove(enemies[i]);
+                }
+            }
+        }
+    }
+}
---
+++ GodsOfCalamity/Meteor.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace GodsOfCalamity
+{
+    public sealed partial class Meteor : UserControl
+    {
+        public int AreaWidth { get; set; }
+        public Windows.Foundation.Point Location { get; set; }
+        public double Velocity;
+        private readonly Random randomizer = new Random();
+        private int direction;
+        private int directionCount = 0;
+
+        public Meteor()
+        {
+            this.InitializeComponent();
+            Velocity = randomizer.Next(1, 3);
+        }
+
+        public void Move()
+        {
+            int move;
+
+            //randomize move direction
+            if (directionCount == 0)
+            {
+                direction = randomizer.Next(1, 3);
+            }
+
+            if (direction == 1){
+                move = -1;
+            }
+            else
+            {
+                move = 1;
+            }
+            directionCount++;
+
+            //Change direction every 30 count
+            if (directionCount > 30)
+            {
+                directionCount = 0;
+            }
+
+            //Check that we don't go through walls
+            if (Location.X + direction < 0)
+            {
+                move = 0;
+            }
+            if (Location.X + direction > AreaWidth)
+            {
+                move = AreaWidth;
+            }
+
+            //Set new location
+            Location = new Windows.Foundation.Point(Location.X + move, Location.Y + Velocity);
+        }
+    }
+}
